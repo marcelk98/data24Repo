24a6f056ea28760c614fa174d210f48faa40c0ca
marcelk98 <mkrzyzanowski@spartaglobal.com>
2021-10-14 16:13:31 +0100

    S3 project files

--- BOTO task/load_data.py
+++ BOTO task/load_data.py
@@ -3,4 +3,4 @@ from app import *
 
 #function to load the new .csv file into the specified S3 bucket
 def load_data():
-    s3_client.upload_file(Filename='marcel_fish_test', Bucket=bucket_name, Key='Data100/fish/marcel_fish.csv')
+    s3_client.upload_file(Filename='marcel_fish.csv', Bucket=bucket_name, Key='Data100/fish/marcel_fish.csv')
